e68b4c4093e2bbfadd7b047413f5cb47ea04fbe1
zahdehv <107417716+zahdehyv@users.noreply.github.com>
2024-09-05 13:08:14 -0400

    start

--- one_battle.py
+++ one_battle.py
@@ -6,6 +6,6 @@ for i in range(2):
     teams.append(sim.dict_to_team_set(generate_team()))
 
 battle = sim.Battle('single', 'Nic', teams[0], 'Sam', teams[1], debug=True)
-print(battle.p1.pokemon)
+# print(*battle.p1.pokemon, sep="\n")
 
 sim.run(battle)
